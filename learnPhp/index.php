5c7bf875c1e91bb0cdfc7156763ce441c20e0bad
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-07-27 18:45:31 +0300

    don't work

--- learnPhp/index.php
+++ learnPhp/index.php
@@ -5,15 +5,15 @@
 	<title>FormProcessing</title>
 
 <body>
-	<form name="test" action="" method="post">
+	<form name="test" action="check.php" method="post">
 		<label>Name: </label><br />
-		<input type="text" Name="name" placeholder="Name" /><br/>
+		<input type="text" name="name" placeholder="Name" /><br/>
 		<label>Email: </label><br />
 		<input type="text" name="email" placeholder="Email" /><br/>
 		<label>Messages: </label><br />
 		<textarea type="message" style="height: 100px; background-color: powderblue;" cols="40" rows="50"></textarea>
 		<br />
-		<input type="submit" name="bows"/>
+		<input type="submit" name="done" />
 	</form>
 </body>
 </head>
