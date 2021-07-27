0fead8c07d9e2b9e022870c18ff979458d27bc62
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-07-27 18:38:22 +0300

    form

--- learnPhp/index.php
+++ learnPhp/index.php
@@ -1,6 +1,19 @@
-<?php
-	$title = 'CenterPage';
-	require "header.php";
-	echo "Telo Documenta";
-	include "footer.php";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>FormProcessing</title>
+
+<body>
+	<form name="test" action="" method="post">
+		<label>Name: </label><br />
+		<input type="text" Name="name" placeholder="Name" /><br/>
+		<label>Email: </label><br />
+		<input type="text" name="email" placeholder="Email" /><br/>
+		<label>Messages: </label><br />
+		<textarea type="message" style="height: 100px; background-color: powderblue;" cols="40" rows="50"></textarea>
+		<br />
+		<input type="submit" name="bows"/>
+	</form>
+</body>
+</head>
