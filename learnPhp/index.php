4ede13f77d8e300d37a41668c98c0fc80b7189e9
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-07-28 15:46:55 +0300

    name

--- learnPhp/index.php
+++ learnPhp/index.php
@@ -5,15 +5,14 @@
 	<title>FormProcessing</title>
 
 <body>
-	<form name="test" action="check.php" method="post">
+	<form name="test" action="check.php" method="POST">
 		<label>Name: </label><br />
 		<input type="text" name="name" placeholder="Name" /><br />
 		<label>Email: </label><br />
 		<input type="text" name="email" placeholder="Email" /><br />
 		<label>Messages: </label><br />
-		<textarea type="message" cols="20" rows="10"></textarea>
-		<br />
-		<input type="submit" name="done" value="Отправить" />
+		<textarea name="message" cols="20" rows="10"></textarea><br />
+		<input type="submit" name="done"  value="ckick" />
 	</form>
 </body>
 </head>
